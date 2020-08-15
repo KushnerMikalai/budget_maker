f1ccfc6ce1e5e85d8748de9e2ae2364a9077af25
KushnerMikalai <nickolaikushner@gmail.com>
2020-08-15 21:53:10 +0300

    feat: client vue.js initial

--- experiment/deno/sever.ts
+++
@@ -1,8 +0,0 @@
-import { App } from './src/app.ts';
-import { config } from './src/config/config.ts';
-import { DatabaseConnection } from './src/config/db.ts';
-
-const app = new App(+config.PORT);
-const db = new DatabaseConnection(config.dbName, config.dbHostUrl);
-db.connect();
-app.listen();
--- experiment/deno/src/utils/404.ts
+++
@@ -1,6 +0,0 @@
-export const notFound = async (ctx: any) => {
-  ctx.response.status = 404;
-  ctx.response.body = {
-    error: 'Not Found',
-  };
-};
